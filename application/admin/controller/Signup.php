ac8b6b001460bb0d274fd058344445aa04a6ef4c
shicw <362559994@qq.com>
2019-03-12 11:31:06 +0800

    完成报名项目的增删改查

--- application/admin/controller/Signup.php
+++ application/admin/controller/Signup.php
@@ -15,6 +15,41 @@ use think\Db;
 class Signup extends AdminBaseController
 {
     /**
+     * 查看报名项目页面
+     * @return mixed
+     */
+    public function index(){
+        //关键字查询
+        $request = input('request.');
+        $keyword = '';
+        $conditions = [];
+        if (!empty($request['keyword'])) {
+            $keyword = $request['keyword'];
+
+            $conditions['id|name'] = ['like', "%$keyword%"];
+        }
+        $rows = Db::name('signup_items')
+            ->field(["FROM_UNIXTIME(create_time,'%Y-%m-%d %H:%i:%s') create_time",
+                "FROM_UNIXTIME(begin_time,'%Y-%m-%d %H:%i:%s') begin_time",
+                "FROM_UNIXTIME(end_time,'%Y-%m-%d %H:%i:%s') end_time",
+                'id','name','description','max_student_count','real_student_count'
+            ])
+            ->where($conditions)
+            ->where(['delete_time'=>0])
+            ->order('create_time desc')
+            ->paginate(15,false, [
+                'query' => [
+                    'keyword' => $keyword,
+                ]
+            ]);
+        $page = $rows->render();
+        $this->assign([
+            'rows' => $rows,
+            'page' => $page
+        ]);
+        return $this->fetch();
+    }
+    /**
      * 新建报名项目页面
      */
     public function add(){
@@ -38,5 +73,55 @@ class Signup extends AdminBaseController
             $this->error($result['msg']);
         }
     }
+    /**
+     * 修改页面
+     * @return mixed
+     */
+    public function edit(){
+        $id = $this->request->param('id');
+        //获取详情
+        $model = new SignupModel();
+        $data = $model->getDetail($id);
+        $this->assign($data);
+        return $this->fetch();
+    }
 
+    /**
+     * 修改提交
+     */
+    public function editPost(){
+        if(!$this->request->isPost()){
+            $this->error('请求失败');
+        }
+        $post = $this->request->post();
+
+        $model = new SignupModel();
+        $post['begin_time'] = strtotime($post['begin_time']);
+        $post['end_time'] = strtotime($post['end_time']);
+        $result = $model->doEdit($post);//添加
+
+        if($result['code']==1){
+            $this->success($result['msg']);
+        }else{
+            $this->error($result['msg']);
+        }
+    }
+    /**
+     * 删除班级
+     */
+    public function delete(){
+        if(!$this->request->isPost()){
+            $this->error('请求失败');
+        }
+        $id = $this->request->post('id');
+
+        $model = new SignupModel();
+        $result = $model->doDelete($id);//删除
+
+        if($result['code']==1){
+            $this->success($result['msg']);
+        }else{
+            $this->error($result['msg']);
+        }
+    }
 }
--- application/admin/model/SignupModel.php
+++ application/admin/model/SignupModel.php
@@ -23,14 +23,57 @@ class SignupModel extends Model
             'description' => $data['description'],
             'max_student_count' => $data['max_student_count'],
             'real_student_count' => 0,
-            'begin_time' => $data['begin_time'],
-            'end_time' => $data['begin_time'],
+            'begin_time' => strtotime($data['begin_time']),
+            'end_time' =>strtotime($data['end_time']),
             'create_time' => time()
         ]);
         if ($result) {
             return ['code' => 1, 'msg' => '添加成功'];
         } else {
             return ['code' => 0, 'msg' => '添加失败'];
+        }
+    }
+    /**
+     * 获取信息
+     * @param $id
+     * @return array
+     */
+    public function getDetail($id){
+        $result = $this->where('id',$id)->find()->getData();
+        return $result;
+    }
+    /**
+     * 删除
+     * @param $id
+     * @return array
+     */
+    public function doDelete($id){
+        $find = $this->field(['id'])->where(['id'=>$id,'delete_time'=>0])->find();
+        if(!$find){
+            return ['code' => 0, 'msg' => '该记录不存在,可能已被删除'];
+        }
+        $result = $this->where('id',$id)->update(['delete_time'=>time()]);
+        if ($result) {
+            return ['code' => 1, 'msg' => '删除成功'];
+        } else {
+            return ['code' => 0, 'msg' => '删除失败'];
+        }
+    }
+    /**
+     * 修改
+     * @param $post
+     * @return array
+     */
+    public function doEdit($post){
+        $find = $this->field(['id'])->where(['id'=>$post['id'],'delete_time'=>0])->find();
+        if(!$find){
+            return ['code' => 0, 'msg' => '该记录不存在,可能已被删除'];
+        }
+        $result = $this->where('id',$post['id'])->update($post);
+        if ($result) {
+            return ['code' => 1, 'msg' => '修改成功'];
+        } else {
+            return ['code' => 0, 'msg' => '没有新的修改信息'];
         }
     }
 }
